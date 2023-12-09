edbdb4e3608d8e0d8c6d3178d5e3f78679abfb52
elsuizo <mnoblia@disroot.org>
2023-12-08 22:46:10 -0300

    update no se que

--- Rust/day2/src/main.rs
+++ Rust/day2/src/main.rs
@@ -1,9 +1,26 @@
+use nom::{
+    bytes::complete::{tag, take_while_m_n},
+    combinator::map_res,
+    sequence::tuple,
+    IResult,
+};
+
+use nom::bytes::complete::take_while;
+use nom::character::is_alphabetic;
+
 use std::error::Error;
 
-enum Cubes {
-    Red,
-    Green,
-    Blue,
+#[derive(Debug, PartialEq)]
+struct Cubes {
+    red: usize,
+    green: usize,
+    blue: usize,
+}
+
+impl Cubes {
+    // fn from(s: &str) -> Self {
+    //
+    // }
 }
 
 struct Game {
@@ -11,19 +28,34 @@ struct Game {
     elements: Vec<Cubes>,
 }
 
-// impl Game {
-//     fn new_from(input: &str) -> Self {
-//     }
+impl Game {
+    fn new_from(lines: &[&str]) -> Self {
+        for line in lines {
+            let games: Vec<_> = line.split(&[';', ':']).collect();
+            // println!("{games:?}");
+            for (game_id, game) in games.iter().enumerate() {
+                println!("{game:?}");
+            }
+        }
+        Game {
+            id: 0,
+            elements: vec![],
+        }
+    }
+}
+
+// "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"
+
+// fn parse_cubes(input: &str) -> IResult<&str, &str> {
+//     let (input, _) = tag(":")(input)?;
+//     Ok(tag(";")(input)?)
+//     // let (input, (red, green, blue)) = (())
 // }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input = std::fs::read_to_string("input_small.txt")?;
 
-    let input: Vec<_> = input.split_terminator("\n").collect();
-
-    let g1: Vec<_> = input[0].split(&[';', ':']).collect();
-
-    println!("{g1:?}");
+    println!("{input:?}");
 
     Ok(())
 }
